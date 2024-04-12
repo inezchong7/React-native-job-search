85021487af6361114a17de99266fb06581ac8c4f
inezchong7 <inezchong7@gmail.com>
2024-04-12 14:39:30 +1200

    popular jobs section done

--- react-native-job-search/components/common/cards/popular/PopularJobCard.jsx
+++ react-native-job-search/components/common/cards/popular/PopularJobCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './popularjobcard.style'
+import {checkImageURL} from '../../../../utils';
 
 const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
   return (
@@ -14,12 +15,25 @@ const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
       >
         <Image 
           // uri = source identifier 
-          source={{uri: item.employer_logo}}
+          source={{uri: checkImageURL(item.employer_logo)
+            // if valid images, display them
+            ? item.employer_logo
+            : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
           resizeMode='contain'
           style={styles.logoImage}
         />
       </TouchableOpacity>
       <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <Text style={styles.location}>
+          {item.job_country}
+        </Text>
+      </View>
     </TouchableOpacity>
 
   )
